593f610ac67e961af424cc62dfd2852b371ade3d
조아 <parkjoah@naver.com>
2025-09-12 10:46:52 +0900

    feat : implement mapMarker with sample data (#9)

--- src/components/myMap/MapArea.jsx
+++ src/components/myMap/MapArea.jsx
@@ -1,7 +1,13 @@
-import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
+import { Map, MapMarker, useKakaoLoader } from "react-kakao-maps-sdk";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../../constants/routes";
 import { useState } from "react";
+import mapMarker from "../../assets/icons/myMap/mapMarker.svg";
+
+const sampleMapData = [
+  { center: { lat: 33.450701, lng: 126.571644 } },
+  { center: { lat: 33.450701, lng: 126.570663 } },
+];
 
 export const MapArea = () => {
   useKakaoLoader();
@@ -31,8 +37,8 @@ export const MapArea = () => {
           <p>메인 홈</p>
         </div>
         <div
-          className=" cursor-pointer z-10 flex gap-2 items-center text-[#3C3C3C] bg-[#FFC982] pl-3 pr-6 py-1.5 font-semibold rounded-[12px]"
-          onClick={() => setOpenSavedList(true)}
+          className=" cursor-pointer z-20 flex gap-2 items-center text-[#3C3C3C] bg-[#FFC982] pl-3 pr-6 py-1.5 font-semibold rounded-[12px]"
+          onClick={() => setOpenSavedList((crr) => !crr)}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -52,7 +58,7 @@ export const MapArea = () => {
       {/* 저장목록 */}
 
       {openSavedList && (
-        <div className="w-[392px] h-full z-15 bg-[#fff] top-0 absolute left-0 pt-[81px] pl-[22px]">
+        <div className="w-[392px] h-full z-10 bg-[#fff] top-0 absolute left-0 pt-[81px] pl-[22px]">
           d
         </div>
       )}
@@ -63,7 +69,21 @@ export const MapArea = () => {
         id="map"
         center={{ lat: 33.450701, lng: 126.570667 }}
         level={3}
-      />
+      >
+        {sampleMapData.map((m, idx) => (
+          <MapMarker
+            position={m.center}
+            key={idx}
+            image={{
+              src: mapMarker,
+              size: {
+                width: 70,
+                height: 70,
+              },
+            }}
+          />
+        ))}
+      </Map>
     </div>
   );
 };
