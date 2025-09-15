4f6c81944e30143e64015a1c48d77053764ad34c
이현진 <leehyunjin1211@gmail.com>
2025-09-15 10:56:58 +0900

    fix: 전체 변경사항 커밋 (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ROUTES } from "./constants/routes";
 import { HomeGuest } from "./pages/HomeGuest";
 import { HomeMember } from "./pages/HomeMemeber";
-<<<<<<< HEAD
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.HOME_GUEST} element={<HomeGuest />} />
-        <Route path={ROUTES.LOGIN} element={<Login />} />
-        <Route path={ROUTES.HOME_MEMBER} element={<HomeMember />} />
-        <Route path={ROUTES.SIGNUP} element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-=======
-import { Login } from "./pages/Login";
-import { SignUp } from "./pages/SignUp";
-import { MyMap } from "./pages/MyMap";
+import  Login  from "./pages/Login";
+import  SignUp  from "./pages/SignUp";
+import { MyMap }  from "./pages/MyMap";
 
 function App() {
   return (
@@ -34,7 +19,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
->>>>>>> fcc356924a86542761afb0dace3edf43f8324615
   );
 }
 
